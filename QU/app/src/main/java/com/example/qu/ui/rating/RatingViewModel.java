2155d712fde8c25e619fe04e670c529b45c0649f
Anastasia Bondarenko <ne7sgreen@gmail.com>
2021-04-05 12:04:27 +0300

    change a bit main activity view

---
+++ QU/app/src/main/java/com/example/qu/ui/rating/RatingViewModel.java
@@ -0,0 +1,19 @@
+package com.example.qu.ui.rating;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class RatingViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public RatingViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is rating fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
