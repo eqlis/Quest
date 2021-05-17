89177ce06520d58424014286addaf4c4512bbd44
Anastasia Bondarenko <ne7sgreen@gmail.com>
2021-05-17 11:59:19 +0300

    simplify the architecture

---
+++ QU/app/src/main/java/com/example/qu/CreateQuestActivity.java
@@ -0,0 +1,14 @@
+package com.example.qu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateQuestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_quest);
+    }
+}
